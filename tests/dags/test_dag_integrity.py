684b2ee606c156b4690ed7e5e53d1d91bf5910da
m-p-esser <m-esser@mail.de>
2022-01-02 18:20:24 +0100

    Add test to check data integrity of DAGs

--- tests/dags/test_dag_integrity.py
+++ tests/dags/test_dag_integrity.py
@@ -1,6 +1,7 @@
 """ General Validation tests for DAGs """
 
 from airflow.models import DagBag
+from airflow.utils.dag_cycle_tester import test_cycle
 
 
 def test_no_import_errors():
@@ -8,16 +9,7 @@ def test_no_import_errors():
     assert len(dag_bag.import_errors) == 0, "No Import Failures"
 
 
-# def test_all_dags_have_tags():
-#     dag_bag = DagBag(dag_folder="dags/", include_examples=False)
-#     for dag_id, dag in dag_bag.dags.items():
-#         error_message = f"{dag_id} in {dag.full_filepath} has no tags"
-#         assert dag.tags, error_message
-
-
-# def test_retries_present():
-#     dag_bag = DagBag(dag_folder="dags/", include_examples=False)
-#     for dag in dag_bag.dags:
-#         retries = dag_bag.dags[dag].default_args.get("retries", [])
-#         error_msg = "Retries not set to 2 for DAG {id}".format(id=dag)
-#         assert retries == 2, error_msg
+def test_cycles():
+    dag_bag = DagBag(dag_folder="dags/", include_examples=False)
+    for (dag,) in dag_bag.dags():
+        test_cycle(dag)
