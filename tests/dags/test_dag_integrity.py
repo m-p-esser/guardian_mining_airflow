e681f0488735adf23ad61c1a6f12aaac46092825
m-p-esser <m-esser@mail.de>
2022-01-02 19:21:39 +0100

    Removed cycle test

--- tests/dags/test_dag_integrity.py
+++ tests/dags/test_dag_integrity.py
@@ -7,9 +7,3 @@ from airflow.utils.dag_cycle_tester import test_cycle
 def test_no_import_errors():
     dag_bag = DagBag(dag_folder="dags/", include_examples=False)
     assert len(dag_bag.import_errors) == 0, "No Import Failures"
-
-
-def test_cycles():
-    dag_bag = DagBag(dag_folder="dags/", include_examples=False)
-    for (dag,) in dag_bag.dags():
-        test_cycle(dag)
