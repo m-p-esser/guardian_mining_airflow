3e3683b4220e4a61cf9c95a9924aeea94a4ce4f0
m-p-esser <m-esser@mail.de>
2022-01-02 18:21:20 +0100

    Add fixtures and fixture helpers to Pytest configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ def test_dag():
     return DAG(
         "test_dag",
         default_args={"owner": "airflow", "start_date": datetime.datetime(2020, 1, 1)},
-        schedule_interval=datetime.timedelta(days=1),
+        schedule_interval="@once",
     )
 
 
