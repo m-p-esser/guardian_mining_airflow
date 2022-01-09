e62e6b4151fed83c69ec87be28ebc2a4a0f16354
m-p-esser <m-esser@mail.de>
2022-01-09 17:35:59 +0100

    changed the way the title is defined in Pandas Profile

--- dags/src/profiling.py
+++ dags/src/profiling.py
@@ -1,11 +1,12 @@
 """Module for Profiling functions """
 
 from pandas_profiling import ProfileReport
+import pandas as pd
 
 
-def create_pandas_profile(df, output_file_prefix, **parameter):
+def create_pandas_profile(df: pd.DataFrame, file_name: str, **parameter):
     """Create Pandas Profile from Dataframe"""
-    output_file_prefix = output_file_prefix.replace("_", " ").upper()
-    title = f"{output_file_prefix} Profile Report"
+    file_name_upper = file_name.replace("_", " ").upper()
+    title = f"{file_name_upper} Profile Report"
     profile = ProfileReport(df, title, **parameter)
     return profile
